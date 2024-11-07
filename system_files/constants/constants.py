67a8bc452ef5045d6d471a381f002417c90dd24d
EugenyCat <eugenycat@gmail.com>
2024-11-07 22:57:53 +0200

    big refactoring

--- system_files/constants/constants.py
+++ system_files/constants/constants.py
@@ -2,5 +2,5 @@ CH_BACKUPS_PATH = '/var/lib/clickhouse/backups/'
 CH_OLD_BACKUPS_SUBDIRECTORY = 'prev_backups/'
 GRAFANA_BACKUP_DIR = "/opt/airflow/system_files/provisioning/dashboards"
 
-DEFAULT_DATA_START_LOAD ='2015-01-01 00:00:00'
+DEFAULT_DATA_START_LOAD = '2015-01-01 00:00:00'
 ALLOWED_INTERVALS_LOAD = '1h,1d,3d,1w,1M'
